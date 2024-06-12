ba5bd55abf2588e834d1d2d6ffb54c8f7152cf3c
Memo Martell <gucasta@me.com>
2024-06-12 12:51:35 -0600

    Fixex Python Lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -26,13 +26,14 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
     # Get username and password from request.POST dictionary
     data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
+    username = data["userName"]
+    password = data["password"]
     # Try to check if provide credential can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
@@ -42,11 +43,13 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     logout(request)
-    data = {"userName":""}
+    data = {"userName": ""}
     return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
@@ -54,11 +57,11 @@ def registration(request):
     context = {}
 
     data = json.loads(request.body)
-    username = data['username']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data ['email']
+    username = data["username"]
+    password = data["password"]
+    first_name = data["firstName"]
+    last_name = data["lastName"]
+    email = data["email"]
     username_exist = False
     email_exist = False
     try:
@@ -68,7 +71,13 @@ def registration(request):
         logger.debug("{} is new user".format(username))
 
     if not username_exist:
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email,
+        )
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
@@ -80,22 +89,23 @@ def registration(request):
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    
-    if(count == 0):
+
+    if count == 0:
         initiate()
-    
-    car_models = CarModel.objects.select_related('car_make')
+
+    car_models = CarModel.objects.select_related("car_make")
     cars = []
-    
+
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    
+
     return JsonResponse({"CarModels": cars})
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-def get_dealerships(request, state = "ALL"):
-    if(state == "ALL"):
+def get_dealerships(request, state="ALL"):
+    if state == "ALL":
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/" + state
@@ -104,14 +114,14 @@ def get_dealerships(request, state = "ALL"):
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
-    if(dealer_id):
+def get_dealer_reviews(request, dealer_id):
+    if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
+            response = analyze_review_sentiments(review_detail["review"])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
+            review_detail["sentiment"] = response["sentiment"]
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
@@ -119,19 +129,20 @@ def get_dealer_reviews(request,dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-    if(dealer_id):
+    if dealer_id:
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
+
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if request.user.is_anonymous == False:
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({ "status": 200 })
+            return JsonResponse({"status": 200})
         except:
-            return JsonResponse({ "status": 400, "message": "Unauthorized"})
+            return JsonResponse({"status": 400, "message": "Unauthorized"})
